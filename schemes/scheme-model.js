f19a7fb8baa34a738b4021aef7bb2cd648f2a7b0
Erica Girges <ericafiorenza@gmail.com>
2020-10-07 20:02:51 -0700

    progress on database methods

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,18 +1,31 @@
 const db = require("../data/db-config.js");
 
 module.exports = {
-    find,
-    add,
-    findById,
-    update,
-    remove,
-    findSteps
+  find,
+  add,
+  findById,
+  update,
+  remove,
+  findSteps,
 };
 
 function find() {
-    return db("schemes");
+  return db("schemes");
 }
 
 function findById(id) {
-    return db("schemes").where({id}).first();
+  return db("schemes").where({ id }).first();
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme, id)
+    .then((ids) => {
+      const id = ids[0];
+      return findById(id);
+    });
+}
+
+function update(id, changes) {
+  return db("schemes").where({ id }).update(changes);
 }
